557898b01218a05d4b1a1bca2f5f241ee91f4fc5
Tomis Koumides <tomiskoumides249@gmail.com>
2022-12-14 17:18:56 +0200

    0.0.2 Commit about passport changes on logout functionality

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
-const users = require('../controllers/users');
+const passport = require("passport");
+const catchAsync = require("../utils/catchAsync");
+const User = require("../models/user");
+const users = require("../controllers/users");
 
-router.route('/register')
-    .get(users.renderRegister)
-    .post(catchAsync(users.register));
+router
+  .route("/register")
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 
-router.route('/login')
-    .get(users.renderLogin)
-    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login
+  );
 
-router.get('/logout', users.logout)
+router.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "Goodbye!");
+    res.redirect("/campgrounds");
+  });
+});
 
 module.exports = router;
