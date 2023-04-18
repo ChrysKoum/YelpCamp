5be854e34661c2622c831df4d863438088abbfc3
ChrysKoum <chyrkoum@gmail.com>
2023-04-18 04:21:32 +0300

    preloader test

--- app.js
+++ app.js
@@ -25,10 +25,7 @@ const MongoStore = require("connect-mongo");
 
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
-mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
